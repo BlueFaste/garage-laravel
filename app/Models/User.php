182fc9b816049be383664dad692c0bff82ca8e01
Caroline Fassot <fassot.caroline@gmail.com>
2021-01-26 16:55:09 +0100

    creation model (model pivot wrong)

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+
+    public function user_vehicle()
+    {
+        return $this->hasMany(UserVehicle::class);
+
+    }
 }
